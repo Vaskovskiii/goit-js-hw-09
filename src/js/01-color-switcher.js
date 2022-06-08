9fadb8c884b735fa04861a65badd1618568c0163
Vaskovskiii <rostikfutu4e250303@ukr.net>
2022-06-08 15:44:52 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,30 +1,30 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 const refs = {
   bodyEl: document.querySelector('body'),
   btn: document.querySelector('[data-start]'),
   btnStop: document.querySelector('[data-stop]'),
-}
+};
 
 refs.btn.addEventListener('click', () => {
-  myStartFunction()
-  refs.btn.disabled = true
-  refs.btnStop.disabled = false
-})
+  myStartFunction();
+  refs.btn.disabled = true;
+  refs.btnStop.disabled = false;
+});
 
 refs.btnStop.addEventListener('click', () => {
-  refs.btnStop.disabled = true
-  refs.btn.disabled = false
-  myStopFunction()
-})
+  refs.btnStop.disabled = true;
+  refs.btn.disabled = false;
+  myStopFunction();
+});
 
-let timerId
+let timerId;
 function myStartFunction() {
   timerId = setInterval(() => {
-    refs.bodyEl.style.backgroundColor = getRandomHexColor()
-  }, 1000)
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 }
 function myStopFunction() {
-  clearInterval(timerId)
+  clearInterval(timerId);
 }
